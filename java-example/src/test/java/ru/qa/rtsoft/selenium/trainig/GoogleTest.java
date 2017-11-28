dcee62f04d0e678b37a7eee6d3e1f4725fd732bd
subkorvin <portrait@mail.ru>
2017-11-28 19:40:51 +0300

    first test (google)

---
+++ java-example/src/test/java/ru/qa/rtsoft/selenium/trainig/GoogleTest.java
@@ -0,0 +1,37 @@
+package ru.qa.rtsoft.selenium.trainig;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class GoogleTest {
+
+  private WebDriver driver;
+  private WebDriverWait wait;
+
+  @Before
+  public void start(){
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 10);
+  }
+
+  @Test
+  public void googleTest(){
+    driver.get("http://www.google.com");
+    driver.findElement(By.name("q")).sendKeys("webdriver");
+    driver.findElement(By.name("btnK")).click();
+    wait.until(titleIs("webdriver - поиск в Google"));
+  }
+
+  @After
+  public void stop(){
+    driver.quit();
+    driver = null;
+  }
+}
