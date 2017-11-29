8e2b8bc7deedad200af99586ccaa7a8dc711c8dc
subkorvin <portrait@mail.ru>
2017-11-29 16:37:57 +0300

    Лекция 2, задание 2, добавлен модуль логина в админку веб-магазина

---
+++ litecart/src/test/java/ru/qa/rtsoft/selenium/training/AdminLoginTest.java
@@ -0,0 +1,35 @@
+package ru.qa.rtsoft.selenium.training;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AdminLoginTest {
+
+  private WebDriver driver;
+
+  @Before
+  public void start(){
+    driver = new ChromeDriver();
+    //driver = new FirefoxDriver();
+    driver.manage().window().maximize();
+  }
+
+  @Test
+  public void adminLoginTest() throws InterruptedException {
+    driver.navigate().to("http://localhost:8080/litecart/admin");
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("login")).click();
+    Thread.sleep(10000); // пока без проверок для визуального контроля
+  }
+
+  @After
+  public void stop(){
+    driver.quit();
+    driver = null;
+  }
+}
