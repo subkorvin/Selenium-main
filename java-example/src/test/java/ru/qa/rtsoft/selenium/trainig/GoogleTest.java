fb513caeff68a3e4fb858591840b5a1014521b02
subkorvin <portrait@mail.ru>
2017-12-01 19:33:45 +0300

    Лекция 3

--- java-example/src/test/java/ru/qa/rtsoft/selenium/trainig/GoogleTest.java
+++ java-example/src/test/java/ru/qa/rtsoft/selenium/trainig/GoogleTest.java
@@ -3,15 +3,18 @@ package ru.qa.rtsoft.selenium.trainig;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import java.io.File;
 
 public class GoogleTest {
 
@@ -20,22 +23,40 @@ public class GoogleTest {
 
   @Before
   public void start(){
-    driver = new ChromeDriver();
+
+    /*ChromeOptions options = new ChromeOptions();
+    options.addArguments("start-maximized");
+    driver = new ChromeDriver(options);*/
+
+    //DesiredCapabilities caps = new DesiredCapabilities();
+    //caps.setCapability("unexpectedAlertBehaviour", "dismiss");
+    //driver = new EdgeDriver(caps);
+    //System.out.println(((HasCapabilities) driver).getCapabilities());
+
+    DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setCapability(FirefoxDriver.MARIONETTE, false);
+    driver = new FirefoxDriver(new FirefoxBinary(new File("\"C:\\Program Files\\Mozilla Firefox ESR\\firefox.exe\"")), new FirefoxProfile(), caps);
+    System.out.println(((HasCapabilities) driver).getCapabilities());
+
+    //driver = new ChromeDriver();
     //driver = new FirefoxDriver();
-    driver.manage().window().maximize();
+    //driver = new EdgeDriver();
+    //driver = new InternetExplorerDriver();
+    //driver = new OperaDriver();
+    //driver.manage().window().maximize();
     wait = new WebDriverWait(driver, 10);
   }
 
   @Test
-  public void googleTest() throws InterruptedException {
+  public void googleTest(){
     driver.get("http://www.google.com");
-    driver.findElement(By.name("q")).sendKeys("webdriver");
+    //driver.findElement(By.name("q")).sendKeys("webdriver");
     //driver.findElement(By.name("btnK")).click();
     //driver.findElement(By.name("q")).sendKeys(Keys.ENTER); //нечестно, заходим по Enter
     //driver.findElement(By.xpath("//div[@class='jsb']/center/input[@value='Поиск в Google']")).click();
     //driver.findElement(By.cssSelector("form[class='tsf'] input[name='btnK']")).click();
-    driver.findElement(By.xpath("//form[@id='tsf']/div[2]/div[3]/center/input")).click(); //ура
-    wait.until(titleIs("webdriver - Поиск в Google"));
+    //driver.findElement(By.xpath("//form[@id='tsf']/div[2]/div[3]/center/input")).click(); //ура
+    //wait.until(titleIs("webdriver - Поиск в Google"));
   }
 
   @After
