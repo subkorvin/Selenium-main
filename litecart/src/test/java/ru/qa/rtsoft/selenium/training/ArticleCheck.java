1c592e8defe51e24e7f7e11d1a457c6d27ef4b60
subkorvin <portrait@mail.ru>
2017-12-12 01:02:26 +0300

    задание 10 продолжение

--- litecart/src/test/java/ru/qa/rtsoft/selenium/training/ArticleCheck.java
+++ litecart/src/test/java/ru/qa/rtsoft/selenium/training/ArticleCheck.java
@@ -12,14 +12,27 @@ public class ArticleCheck extends TestBase {
     String articleNameFromMain = driver.findElement(By.cssSelector("#box-campaigns .name")).getAttribute("textContent");
     String regularPriceFromMain = driver.findElement(By.cssSelector("#box-campaigns .regular-price")).getAttribute("textContent");
     String campaignPriceFromMain = driver.findElement(By.cssSelector("#box-campaigns .campaign-price")).getAttribute("textContent");
-    String n = driver.findElement(By.cssSelector("#box-campaigns .regular-price")).getCssValue("color");
-    VerbalExpression regex = VerbalExpression.regex().capt().find("(").digit().count(3).endCapture().build();
-    String r = regex.getText(n);
+    //String n = driver.findElement(By.cssSelector("#box-campaigns .regular-price")).getCssValue("color");
+    //String n = driver.findElement(By.cssSelector("#box-campaigns .campaign-price")).getCssValue("color");
+    String n = driver.findElement(By.cssSelector("#box-most-popular div[class='sticker new']")).getCssValue("background-color");
+    //VerbalExpression regex = VerbalExpression.regex().find("(").capture().digit().count(3).endCapture().build();
+    VerbalExpression regex = VerbalExpression.regex().find("(")
+            .capture().digit().count(0, 3).endCapture()
+            .then(", ")
+            .capture().digit().count(0, 3).endCapture()
+            .then(", ")
+            .capture().digit().count(0, 3).endCapture()
+            .build();
+    String r = regex.getText(n, 1);
+    String g = regex.getText(n, 2);
+    String b = regex.getText(n, 3);
 
 
 
     //System.out.println(articleNameFromMain + " " + regularPriceFromMain + " " + campaignPriceFromMain);
-     //System.out.println(n);
+    System.out.println(n);
     System.out.println(r);
+    System.out.println(g);
+    System.out.println(b);
   }
 }
