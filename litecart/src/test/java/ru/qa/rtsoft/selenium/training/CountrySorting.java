9b7227c7a3019c4270d721d9bd95a29f576a772b
subkorvin <portrait@mail.ru>
2017-12-11 15:02:52 +0300

    Лекция 5 задание 9

--- litecart/src/test/java/ru/qa/rtsoft/selenium/training/CountrySorting.java
+++ litecart/src/test/java/ru/qa/rtsoft/selenium/training/CountrySorting.java
@@ -1,10 +1,12 @@
 package ru.qa.rtsoft.selenium.training;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CountrySorting extends TestBase {
@@ -15,14 +17,23 @@ public class CountrySorting extends TestBase {
     login.adminLoginTest();
     driver.navigate().to("http://localhost:8080/litecart/admin/?app=countries&doc=countries");
     List<WebElement> elementslist = driver.findElements(By.cssSelector("form[name=countries_form] .dataTable .row"));
-    List<String> zones = new ArrayList<>();
     List<String> countries = new ArrayList<>();
+    List<String> countriessorted = new ArrayList<>();
     for (WebElement element : elementslist) {
       String country = element.findElement(By.cssSelector("a")).getAttribute("textContent");
       countries.add(country);
+      countriessorted.add(country);
     }
+    Collections.sort(countriessorted);
+    Assert.assertTrue(countries.equals(countriessorted));
+  }
 
-
+  @Test
+  public void geoZonesSorting() {
+    AdminLoginTest login = new AdminLoginTest();
+    login.adminLoginTest();
+    driver.navigate().to("http://localhost:8080/litecart/admin/?app=countries&doc=countries");
+    List<String> zones = new ArrayList<>();
     List<WebElement> rows = driver.findElements(By.cssSelector("form[name=countries_form] .dataTable .row"));
     for (WebElement row : rows) {
       String zone = row.findElement(By.cssSelector("td:nth-child(6)")).getAttribute("outerText");
@@ -30,18 +41,18 @@ public class CountrySorting extends TestBase {
         String zonelink = row.findElement(By.cssSelector("td > a")).getAttribute("href");
         zones.add(zonelink);
       }
-
-
-
-//      for (WebElement cell : rows){
-//        String zone = cell.getAttribute("outerText");
-//        if (zone.equals("0")){
-//          return;
-//        }
-//        String zonelink = element.findElement(By.cssSelector("td > a")).getAttribute("href");
-//        zones.add(zonelink);
-//      }
     }
-    System.out.println(zones);
+    for (String zone : zones){
+      driver.navigate().to(zone);
+      List<WebElement> geozones = driver.findElements(By.cssSelector(".dataTable tr td:nth-child(3) input[type='hidden']"));
+      List<String> geozonenames = new ArrayList<>();
+      List<String> geozonenamessorted = new ArrayList<>();
+      for (WebElement geozone : geozones){
+        geozonenames.add(geozone.getAttribute("value"));
+        geozonenamessorted.add(geozone.getAttribute("value"));
+        Collections.sort(geozonenamessorted);
+        Assert.assertTrue(geozonenames.equals(geozonenamessorted));
+      }
+    }
   }
 }
