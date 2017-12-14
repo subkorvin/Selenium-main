def21d9c85e3fae0a6c29f2711f67c9becec8820
subkorvin <portrait@mail.ru>
2017-12-14 15:13:04 +0300

    Лекция 6, задание 11

--- litecart/src/test/java/ru/qa/rtsoft/selenium/training/NewUserRegistration.java
+++ litecart/src/test/java/ru/qa/rtsoft/selenium/training/NewUserRegistration.java
@@ -2,26 +2,52 @@ package ru.qa.rtsoft.selenium.training;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+
 
 public class NewUserRegistration extends TestBase {
 
-  String taxId = "000000000";
-  String company = "RTSoft";
-  String firstName = "Ivan";
-  String lastName = "Pupkin";
-  String address = "Leninsky tupik";
-  String postcode = "12345";
-  String city = "Wrotslaw";
-  String email = "111@1.ru";
-  String phone = "1234567";
-  String password = "1";
+  private String taxId = "000000000";
+  private String company = "RTSoft";
+  private String firstName = "Ivan";
+  private String lastName = "Pupkin";
+  private String address = "Leninsky tupik";
+  private String postcode = "12345";
+  private String city = "Wrotslaw";
+  private String email = "444@1.ru";
+  private String phone = "1234567";
+  private String password = "1";
 
 
   @Test
-  public void newUserRegistration() throws InterruptedException {
+  public void newUserRegistration() {
     driver.navigate().to("http://localhost:8080/litecart");
     driver.findElement(By.cssSelector("form[name='login_form'] a")).click();
     driver.findElement(By.cssSelector("form[name='customer_form'] input[name='tax_id']")).sendKeys(taxId);
-    Thread.sleep(3000);
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='company']")).sendKeys(company);
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='firstname']")).sendKeys(firstName);
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='lastname']")).sendKeys(lastName);
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='address1']")).sendKeys(address);
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='postcode']")).sendKeys(postcode);
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='city']")).sendKeys(city);
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='email']")).sendKeys(email);
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='phone']")).sendKeys(phone);
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='password']")).sendKeys(password);
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='confirmed_password']")).sendKeys(password);
+    WebElement country = driver.findElement(By.cssSelector("form[name='customer_form'] select[name='country_code']"));
+    WebElement state = driver.findElement(By.cssSelector("form[name='customer_form'] select[name='zone_code']"));
+    Select countrySelect = new Select(country);
+    Select stateSelect = new Select(state);
+    countrySelect.selectByValue("US");
+    stateSelect.selectByValue("CO");
+    driver.findElement(By.cssSelector("form[name='customer_form'] input[name='newsletter']")).click();
+    driver.findElement(By.cssSelector("form[name='customer_form'] button[name='create_account']")).click();
+    driver.findElement(By.cssSelector("#box-account li:nth-child(4) a")).click(); //logout
+    driver.findElement(By.cssSelector("#box-account-login input[name='email']")).sendKeys(email);
+    driver.findElement(By.cssSelector("#box-account-login input[name='password']")).sendKeys(password);
+    driver.findElement(By.cssSelector("form[name='login_form'] button[name='login']")).click();
+    driver.findElement(By.cssSelector("#box-account li:nth-child(4) a")).click();
   }
 }
